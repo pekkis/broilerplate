c470f4ad2e90e3b87c9528a07932709aef792a32
Mikko Forsström <pekkisx@gmail.com>
2018-03-07 13:26:42 +0200

    Fixore.

--- packages/broilerplate/src/features/babelPolyfillFeature.js
+++ packages/broilerplate/src/features/babelPolyfillFeature.js
@@ -1,14 +1,13 @@
 const baseFeature = require("../baseFeature");
 
-const envs = ["production"];
-
 module.exports = {
   ...baseFeature,
   name: () => "babelPolyfillFeature",
   overrideBase: (values, env, target, paths) => {
-    if (!envs.includes(env)) {
+    if (target === "server" || env === "development") {
       return values;
     }
+
     return values.updateIn(["entry", "client"], e =>
       e.unshift("babel-polyfill")
     );
