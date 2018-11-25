0c6f104de40bc7f8ab29b7e5ef6613ee8a677a29
Mikko Forsström <pekkisx@gmail.com>
2018-11-25 18:12:18 +0200

    Fixore minore.

--- packages/broilerplate-postcss/src/index.js
+++ packages/broilerplate-postcss/src/index.js
@@ -3,12 +3,13 @@ const path = require("path");
 const postCssLoader = require("./postCssLoader");
 const { createFeature } = require("@dr-kobros/broilerplate/lib/extend");
 
-module.exports = createFeature({
-  name: () => "postCssFeature",
-  files: paths =>
-    List.of({
-      source: path.join(__dirname, "../files/postcss.config.js"),
-      target: path.join(paths.get("root"), "postcss.config.js")
-    }),
-  loaders: () => OrderedSet.of(postCssLoader)
-});
+module.exports = config =>
+  createFeature({
+    name: () => "postCssFeature",
+    files: paths =>
+      List.of({
+        source: path.join(__dirname, "../files/postcss.config.js"),
+        target: path.join(paths.get("root"), "postcss.config.js")
+      }),
+    loaders: () => OrderedSet.of(postCssLoader())
+  });
