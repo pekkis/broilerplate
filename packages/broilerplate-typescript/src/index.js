01ff5eafe3a81515f30eaa2500342e49f79e6ac4
Mikko Forsström <pekkisx@gmail.com>
2019-09-13 20:20:28 +0300

    Added actual TypeScript feature.

--- packages/broilerplate-typescript/src/index.js
+++ packages/broilerplate-typescript/src/index.js
@@ -1,26 +1,36 @@
-const { createFeature } = require("@dr-kobros/broilerplate/lib/extend");
+const {
+  createFeature,
+  createLoader
+} = require("@dr-kobros/broilerplate/lib/extend");
+const { fromJS, List } = require("immutable");
+const babelOptions = require("@dr-kobros/broilerplate/lib/loaders/babelOptions");
+
+const loader = createLoader({
+  name: () => "typeScriptLoader",
+  options: (env, target, paths, options) => {
+    return fromJS({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: babelOptions(env, target, paths, options)
+        },
+        {
+          loader: "ts-loader"
+        }
+      ],
+      exclude: [paths.get("modules")]
+    });
+  }
+});
 
 module.exports = config =>
   createFeature({
     name: () => "typeScriptFeature",
-    overrideLoader: loader => {
-      if (loader.name() !== "babelLoader") {
-        return loader;
-      }
-
-      return {
-        ...loader,
-        options: (env, target, paths, options) =>
-          loader
-            .options(env, target, paths, options)
-            .updateIn(["use", 0, "options", "plugins"], p =>
-              p.push([
-                "babel-plugin-styled-components",
-                {
-                  ssr: true
-                }
-              ])
-            )
-      };
+    loaders: () => List.of(loader),
+    overrideBase: base => {
+      return base
+        .updateIn(["resolve", "extensions"], e => e.concat([".ts", ".tsx"]))
+        .setIn(["entry", "client"], "./client.tsx");
     }
   });
